fffa77d7e8030a01167ac623516e79e1798365fc
Paola Guitian <poliguitian@gmail.com>
2019-08-12 19:00:24 -0400

    add FileReader and turn image into Blob

--- packages/web/src/components/Dropzone.tsx
+++ packages/web/src/components/Dropzone.tsx
@@ -1,10 +1,22 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 export default function MyDropzone() {
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
+  const onDrop = useCallback((acceptedFiles:File[]) => {
+    const reader = new FileReader();
+
+    reader.onabort = () => console.log('file reading was aborted')
+    reader.onerror = () => console.log('file reading has failed')
+    reader.onload = () => {
+      // Do whatever you want with the file contents
+      const binaryStr = reader.result
+      console.log(binaryStr)
+    }
+
+    acceptedFiles.forEach(file => reader.readAsBinaryString(file))
+
   }, [])
+
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
